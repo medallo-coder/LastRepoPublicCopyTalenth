d096f27b2777d18a12264a34179f87f6ce17387c
n4amel3es <angel1998dmh@gmail.com>
2025-07-03 14:50:53 -0500

    Tu mensaje describiendo los cambios

--- app/services/perfil_cliente.py
+++ app/services/perfil_cliente.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 import uuid
 
 # Extensiones permitidas
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'tiff', 'ico'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
