2abcdb23018c8a4e8c92a1217bfa9b0e9e0691c4
Tu Nombre <jupahure@gmail.com>
2025-07-13 11:16:44 -0500

    TIEMPO INICIO SESION Y ESTRUCTURACION DESKTOP_SERVICES

--- app/services_desktop/__init__.py
+++ app/services_desktop/__init__.py
@@ -1,3 +1,5 @@
 # /app/services/__init__.py
-from .autenticacion import  iniciar_sesion_admin_service, registrar_admin_service, cerrar_sesion_admin_service,gestionar_usuarios_admin_service 
-from .autenticacion import deshabilitar_cuentas_admin_service, perfil_usuarios_admin_service, gestion_publicaciones_admin_service
+from .autenticacion import  iniciar_sesion_admin_service, registrar_admin_service, cerrar_sesion_admin_service
+from .gestion_publicaciones import   gestion_publicaciones_admin_service
+from .gestionar_usuarios import gestionar_usuarios_admin_service, deshabilitar_cuentas_admin_service
+from .perfil_usuarios import perfil_usuarios_admin_service
---
+++ app/services_desktop/gestionar_usuarios.py
@@ -0,0 +1,97 @@
+from app.models.usuario import Usuario
+from flask import request, session
+from app.services.jwt_service import generar_token, verificar_token
+from app.extensions import db
+
+
+
+def gestionar_usuarios_admin_service():
+    token = session.get('jwt')
+
+      # Si no hay token en sesión, intenta obtenerlo del header Authorization
+    if not token:
+        auth_header = request.headers.get("Authorization")
+        if auth_header and auth_header.startswith("Bearer "):
+            token = auth_header.split(" ")[1]
+    
+    
+    if not token:
+        return {"success": False, "message": "Token  no enviado."}
+    
+    
+    resultado= verificar_token(token)
+    if not resultado["valid"]:
+        return {"success": False, "message": "No estas autenticado "}
+    
+    usuario_id = resultado["payload"].get("usuario_id")
+    
+    usuario_admin = Usuario.query.filter_by(usuario_id=usuario_id).first()
+
+    if not usuario_admin:
+        return{"success": False, "message": "Usuario no encontrado"}
+    
+    if usuario_admin.id_rol != 3:
+        return{"success": False, "message": "No tienes permisos de administrador"}
+    
+    usuarios = Usuario.query.all()
+
+    lista_usuarios = []
+
+    for usuario in usuarios:
+        lista_usuarios.append({
+            "usuario_id": usuario.usuario_id,
+            "fecha_registro": usuario.fecha_registro.strftime("%Y-%m-%d %H:%M:%S")  if usuario.fecha_registro else None,
+            "correo": usuario.correo,
+            "estado": usuario.estado,
+            "token_recuperacion": usuario.token_recuperacion,
+            "intentos_fallidos": usuario.intentos_fallidos,
+            "bloqueado_hasta": usuario.bloqueado_hasta.strftime("%Y-%m-%d %H:%M:%S") if usuario.bloqueado_hasta else None,
+            "id_rol": usuario.id_rol
+        })
+
+    return {"success": True, "usuarios": lista_usuarios}
+
+
+def deshabilitar_cuentas_admin_service(data):
+    token = session.get('jwt')
+
+      # Si no hay token en sesión, intenta obtenerlo del header Authorization
+    if not token:
+        auth_header = request.headers.get("Authorization")
+        if auth_header and auth_header.startswith("Bearer "):
+            token = auth_header.split(" ")[1]
+    
+    if not token:
+        return {"success": True, "message": "Token no enviado"}
+    
+    resultado= verificar_token(token)
+    if not resultado["valid"]:
+        return {"success": False, "message": "No estas autenticado "}
+    
+    usuario_id = resultado["payload"].get("usuario_id")
+    
+    usuario_admin = Usuario.query.filter_by(usuario_id=usuario_id).first()
+    
+    if not usuario_admin:
+        return{"success": False, "message": "Usuario no encontrado"}
+    
+    if usuario_admin.id_rol != 3:
+        return{"success": False, "message": "No tienes permisos de administrador"}
+
+    
+    usuario_id = data.get("usuario_id")
+
+    usuario = Usuario.query.filter_by(usuario_id=usuario_id).first()
+
+    if not usuario:
+        return {"success": False, "message": "No hay ningun usuario"}
+    
+    if usuario.estado == "deshabilitado":
+        return{"success": False, "message": "El usuario ya esta deshabilitado"}
+    
+    if usuario.estado == "activo":
+        usuario.estado = "deshabilitado"
+        db.session.commit()
+
+        return {"success": True, "message": f"El usuario con ID {usuario_id} ha sido deshabilitado"}
+
