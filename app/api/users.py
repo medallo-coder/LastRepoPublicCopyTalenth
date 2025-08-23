fce27492595729dd1a94665d73d8cfdd9cfab335
Tu Nombre <jupahure@gmail.com>
2025-08-23 10:15:03 -0500

    API DE REPORTES

--- app/api/users.py
+++ app/api/users.py
@@ -4,7 +4,7 @@ from app.services_desktop.autenticacion import iniciar_sesion_admin_service, reg
 from app.services_desktop.gestion_publicaciones import  gestion_publicaciones_admin_service, eliminar_publicaciones_admin_service
 from app.services_desktop.gestionar_usuarios import gestionar_usuarios_admin_service, deshabilitar_cuentas_admin_service
 from app.services_desktop.perfil_usuarios  import perfil_usuarios_admin_service 
-#from app.services_desktop.gestion_reportes import gestion_reportes_admin_service
+from app.services_desktop.gestion_reportes import gestion_reportes_admin_service
 # Define the Blueprint for the API
 users_api = Blueprint('users_api', __name__)
 
@@ -64,27 +64,27 @@ def deshabilitar_cuentas_admin():
     return jsonify(resultado), (200 if resultado["success"] else 400)
 
 # Ruta para ver los perfiles de los usuarios
-@users_api.route('perfil_usuarios_admin', methods=['POST'])
+@users_api.route('/perfil_usuarios_admin', methods=['POST'])
 def perfil_usuarios_admin():
     resultado = perfil_usuarios_admin_service()
     return jsonify(resultado), (200 if resultado["success"] else 400)
 
 # Ruta para ver las publicaciones de los usuarios
-@users_api.route('gestion_publicaciones_admin', methods=['POST'])
+@users_api.route('/gestion_publicaciones_admin', methods=['POST'])
 def gestion_publicaciones_admin():
     data= request.get_json() if request.is_json else request.form.to_dict()
     resultado = gestion_publicaciones_admin_service(data)
     return jsonify(resultado), (200 if resultado["success"]else 400) 
 
 # Ruta para eliminar publicaciones de los usuarios 
-@users_api.route('eliminar_publicaciones_admin', methods=['POST'])
+@users_api.route('/eliminar_publicaciones_admin', methods=['POST'])
 def eliminar_publicaciones_admin():
     data= request.get_json() if request.is_json else request.form.to_dict() 
     resultado = eliminar_publicaciones_admin_service(data)
     return jsonify(resultado), (200 if resultado["success"]else 400)   
 
-"""@users_api.route('gestion_reportes_admin', methods=['POST'])
+@users_api.route('/gestion_reportes_admin', methods=['POST'])
 def gestion_reportes_admin():
     data= request.get_json() if request.is_json else request.form.to_dict()
     resultado = gestion_reportes_admin_service(data)
-    return jsonify(resultado), (200 if resultado["success"]else 400) """
+    return jsonify(resultado), (200 if resultado["success"]else 400) 
--- app/services_desktop/gestion_reportes.py
+++ app/services_desktop/gestion_reportes.py
@@ -1,6 +1,7 @@
-"""""
 from app.models.usuario import Usuario
 from app.models.reportes import Reportes
+from app.models.roles import Roles
+from app.models.perfiles import perfiles
 from flask import request, session
 from app.extensions import db
 from app.services.jwt_service import generar_token, verificar_token
@@ -36,8 +37,7 @@ def gestion_reportes_admin_service(data):
     query= Reportes.query
 
     reporte_id = data.get('reporte_id')
-    fecha_reporte = data.get('fecha_reporte')
-    reportador_id = data.get('reporte_id')
+    reportador_id = data.get('reportador_id')
     rol = data.get('tipo_rol')
 
     # Hace un filtro de los reportes que debe traer
@@ -45,8 +45,32 @@ def gestion_reportes_admin_service(data):
     if reporte_id:
         query = query.filter(Reportes.reporte_id==reporte_id)
 
-    if fecha_reporte:
-        query = query
+    
+    if reportador_id:
+        query = query.filter(Reportes.reportador_id==reportador_id)
+    
+    if rol:
+        query= query.join(Reportes.reportador).join(Usuario.rol).filter(Roles.tipo_rol==rol)
+        query = query.distinct()
+    
+    reportes = query.all()
+
+    lista_reportes = []
+
+    for reporte in reportes:
+        lista_reportes.append({
+            "reporte_id": reporte.reporte_id,
+            "descripcion_reporte":reporte.descripcion_reporte,
+            "fecha_reporte": reporte.fecha_reporte,
+            "reportador_id":reporte.reportador_id,
+            "reportado_id":reporte.reportado_id,
+            "reportador_nombre": reporte.reportador.perfiles.primer_nombre,
+            "reportado_nombre": reporte.asignado.perfiles.primer_nombre
+        })
+    return {"success": True, "reportes": lista_reportes}
 
 
-"""
+
+
+
+
